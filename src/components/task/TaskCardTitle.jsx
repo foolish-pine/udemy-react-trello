04fd60db438490b347102cd6badba54ab69f79ed
Matsuda Takuya <matsuda.takuya41@gmail.com>
2022-06-29 00:03:07 +0900

    cp30

---
+++ src/components/task/TaskCardTitle.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export const TaskCardTitle = () => {
+  const CARD_TITLE = "Today";
+
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState(CARD_TITLE);
+
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputCardTitle === "") {
+      setInputCardTitle(CARD_TITLE);
+    }
+    setIsClick(false);
+  };
+
+  const handleBlur = () => {
+    if (inputCardTitle === "") {
+      setInputCardTitle(CARD_TITLE);
+    }
+    setIsClick(false);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            autoFocus
+            maxLength="10"
+          />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
+};
